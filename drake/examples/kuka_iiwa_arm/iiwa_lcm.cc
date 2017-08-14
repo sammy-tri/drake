98eef4c9e735c1fe8d2a44c3c440a9a12dda4e09
Sam Creasey <sam.creasey@tri.global>
2017-08-14 16:45:01 -0400

    send 0 torque values

--- drake/examples/kuka_iiwa_arm/iiwa_lcm.cc
+++ drake/examples/kuka_iiwa_arm/iiwa_lcm.cc
@@ -104,8 +104,9 @@ void IiwaCommandSender::OutputCommand(
   const systems::BasicVector<double>* torques =
       this->EvalVectorInput(context, 1);
   if (torques == nullptr) {
-    command.num_torques = 0;
+    command.num_torques = 7;
     command.joint_torque.clear();
+    command.joint_torque.resize(7, 0);
   } else {
     command.num_torques = num_joints_;
     command.joint_torque.resize(num_joints_);
