a3d3bdab2a0f4da87e7e0869c238de4b18d99804
Rick Cory <rick.cory@tri.global>
2018-04-11 12:59:11 -0700

    Updating run mug sim script.

--- examples/contact_model/run_mug_sim.sh
+++ examples/contact_model/run_mug_sim.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
 # Working parameters
-# ** rk2: rk_dt=6e-6 (based on 8 sphere pad)
+# ** rk2: rk_dt=6e-6 (based on 8 sphere pad), gripper_force=150
 # ** rk3 rk_dt=1e-3, accuracy=1e-3
-# ** timestepping: ts_dt=1e-3
+# ** timestepping: ts_dt=1e-3, gripper_force=42e3
+#				   ts_dt=1e-4, gripper_force=85e4
 #
 ${DRAKE_WORKSPACE}/bazel-bin/examples/contact_model/rigid_mug_gripper \
---simulation_type=timestepping --ts_dt=1e-3 --gripper_force=51e3 \
+--simulation_type=compliant --ts_dt=1e-4 --gripper_force=150 \
 --rk_type=rk2 --rk_dt=6e-6 --accuracy=1e-3 --v_stiction_tolerance=1e-3 \
 --ring_samples=8 --pad_depth=3e-3 --ring_orient=0 --ring_youngs_modulus=-1 \
 --ring_dissipation=-1 --ring_static_friction=-1 --ring_dynamic_friction=-1 \
