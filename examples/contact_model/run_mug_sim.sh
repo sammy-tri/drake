18139ff0061eec03ebcc35b14699ebc7de6a69e0
Rick Cory <rick.cory@tri.global>
2018-05-08 14:34:56 -0700

    Discrete system (timestepping) works.

--- examples/contact_model/run_mug_sim.sh
+++ examples/contact_model/run_mug_sim.sh
@@ -8,10 +8,10 @@
 # Working parameters
 # ** rk2: rk_dt=6e-6 (based on 8 sphere pad), gripper_force=150
 # ** rk3: rk_dt=1e-3, accuracy=1e-3, gripper_force=150
-# ** discrete: ts_dt=1e-6, gripper_force=150e5
+# ** discrete: ts_dt=1e-3, gripper_force=150
 
 ${DRAKE_WORKSPACE}/bazel-bin/examples/contact_model/rigid_mug_gripper \
---system_type=continuous --ts_dt=1e-6 --gripper_force=150 \
+--system_type=continuous --ts_dt=1e-3 --gripper_force=150 \
 --rk_type=rk2 --rk_dt=6e-6 --accuracy=1e-3 --v_stiction_tolerance=1e-3 \
 --ring_samples=8 --pad_depth=3e-3 --ring_orient=0 --ring_youngs_modulus=-1 \
 --ring_dissipation=-1 --ring_static_friction=-1 --ring_dynamic_friction=-1 \
