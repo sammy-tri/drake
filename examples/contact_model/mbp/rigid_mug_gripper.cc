5d785b8cad41caf7f8f60ab8bbee8f6fe3a2b1f8
amcastro-tri <alejandro.castro@tri.global>
2018-05-17 19:26:22 -0400

    Adds the mug model

--- examples/contact_model/mbp/rigid_mug_gripper.cc
+++ examples/contact_model/mbp/rigid_mug_gripper.cc
@@ -19,6 +19,8 @@
 #include "drake/systems/lcm/lcm_publisher_system.h"
 #include "drake/systems/lcm/serializer.h"
 #include "drake/systems/rendering/pose_bundle_to_draw_message.h"
+#include "drake/math/roll_pitch_yaw.h"
+#include "drake/math/rotation_matrix.h"
 
 namespace drake {
 namespace examples {
@@ -26,8 +28,13 @@ namespace multibody {
 namespace cart_pole {
 namespace {
 
+using Eigen::Isometry3d;
+using Eigen::Vector3d;
+using Eigen::AngleAxisd;
 using drake::geometry::SceneGraph;
 using drake::lcm::DrakeLcm;
+using drake::math::RollPitchYaw;
+using drake::math::RotationMatrix;
 using drake::multibody::Body;
 using drake::multibody::multibody_plant::MultibodyPlant;
 using drake::multibody::parsing::AddModelFromSdfFile;
@@ -48,6 +55,17 @@ DEFINE_double(simulation_time, 10.0,
 DEFINE_double(finger_width, 0.1, "The initial distance between the gripper "
     "fingers, when gripper_force > 0.");
 
+// Parameters for posing the mug.
+DEFINE_double(px, 0, "The x-position of the center, bottom of the mug");
+DEFINE_double(py, 0, "The y-position of the center, bottom of the mug");
+DEFINE_double(pz, 0, "The z-position of the center, bottom of the mug");
+DEFINE_double(rx, 0, "The x-rotation of the mug around its origin - the center "
+    "of its bottom (in degrees). Rotation order: X, Y, Z");
+DEFINE_double(ry, 0, "The y-rotation of the mug around its origin - the center "
+    "of its bottom (in degrees). Rotation order: X, Y, Z");
+DEFINE_double(rz, 0, "The z-rotation of the mug around its origin - the center "
+    "of its bottom (in degrees). Rotation order: X, Y, Z");
+
 int do_main() {
   systems::DiagramBuilder<double> builder;
 
@@ -57,6 +75,10 @@ int do_main() {
   MultibodyPlant<double>& plant = *builder.AddSystem<MultibodyPlant>();
   std::string full_name =
       FindResourceOrThrow("drake/examples/contact_model/mbp/schunk_wsg_50.sdf");
+  AddModelFromSdfFile(full_name, &plant, &scene_graph);
+
+  full_name =
+      FindResourceOrThrow("drake/examples/contact_model/mbp/cylinder_mug.sdf");
   AddModelFromSdfFile(full_name, &plant, &scene_graph);
 
   // Add gravity to the model.
@@ -86,6 +108,8 @@ int do_main() {
   builder.Connect(
       plant.get_geometry_poses_output_port(),
       scene_graph.get_source_pose_port(plant.get_source_id().value()));
+  builder.Connect(scene_graph.get_query_output_port(),
+                  plant.get_geometry_query_input_port());
 
   builder.Connect(scene_graph.get_pose_bundle_output_port(),
                   converter.get_input_port(0));
@@ -102,19 +126,30 @@ int do_main() {
   std::unique_ptr<systems::Context<double>> diagram_context =
       diagram->CreateDefaultContext();
   diagram->SetDefaultContext(diagram_context.get());
-  systems::Context<double>& cart_pole_context =
+  systems::Context<double>& plant_context =
       diagram->GetMutableSubsystemContext(plant, diagram_context.get());
 
   // There is no input actuation in this example for the passive dynamics.
-  cart_pole_context.FixInputPort(
+  plant_context.FixInputPort(
       plant.get_actuation_input_port().get_index(), Vector1d(0));
 
   // Get joints so that we can set initial conditions.
   const PrismaticJoint<double>& finger_slider =
       plant.GetJointByName<PrismaticJoint>("finger_sliding_joint");
 
+  // Get mug body so we can set its initial pose.
+  const Body<double>& mug = plant.GetBodyByName("Mug");
+
+  Isometry3d X_WM;
+  Vector3d rpy(FLAGS_rx * M_PI / 180,
+               FLAGS_ry * M_PI / 180,
+               (FLAGS_rz * M_PI / 180) + M_PI);
+  X_WM.linear() = RotationMatrix<double>(RollPitchYaw<double>(rpy)).matrix();
+  X_WM.translation() = Vector3d(FLAGS_px, FLAGS_py, FLAGS_pz);
+  plant.model().SetFreeBodyPoseOrThrow(mug, X_WM, &plant_context);
+
   // Set initial state.
-  finger_slider.set_translation(&cart_pole_context, -FLAGS_finger_width);
+  finger_slider.set_translation(&plant_context, -FLAGS_finger_width);
 
   systems::Simulator<double> simulator(*diagram, std::move(diagram_context));
 
