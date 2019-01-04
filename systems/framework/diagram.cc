be176e39652b3b8f46904959027f561b70214cda
Sam Creasey <sam.creasey@tri.global>
2019-01-04 14:48:49 -0500

    inl some of diagram

--- systems/framework/diagram.cc
+++ systems/framework/diagram.cc
@@ -1,6 +1,7 @@
 #include "drake/systems/framework/diagram.h"
 
 #include "drake/common/default_scalars.h"
+#include "drake/systems/framework/diagram-inl.h"
 
 DRAKE_DEFINE_CLASS_TEMPLATE_INSTANTIATIONS_ON_DEFAULT_SCALARS(
     class ::drake::systems::Diagram)
