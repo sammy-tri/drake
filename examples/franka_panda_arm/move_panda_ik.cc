0764c4af3f80bec60c0ba4d3c11c594a8e367d63
Sam Creasey <sam.creasey@tri.global>
2020-03-09 17:57:52 -0400

    Add another planner and controller

---
+++ examples/franka_panda_arm/move_panda_ik.cc
@@ -0,0 +1,166 @@
+/// @file
+///
+/// Demo of moving the panda's end effector in cartesian space.  This
+/// program creates a plan to move the end effector from the current
+/// position to the location specified on the command line.  The
+/// current calculated position of the end effector is printed before,
+/// during, and after the commanded motion.
+
+#include <map>
+
+#include <bot_core/joint_state_t.hpp>
+#include <bot_core/robot_state_t.hpp>
+#include <gflags/gflags.h>
+#include <lcm/lcm-cpp.hpp>
+#include <robotlocomotion/robot_plan_t.hpp>
+
+#include "drake/common/drake_assert.h"
+#include "drake/common/find_resource.h"
+#include "drake/common/text_logging.h"
+#include "drake/manipulation/planner/constraint_relaxing_ik.h"
+#include "drake/manipulation/util/robot_plan_utils.h"
+#include "drake/math/rigid_transform.h"
+#include "drake/math/roll_pitch_yaw.h"
+#include "drake/math/rotation_matrix.h"
+#include "drake/multibody/parsing/parser.h"
+#include "drake/multibody/plant/multibody_plant.h"
+
+DEFINE_string(urdf, "", "Name of urdf to load");
+DEFINE_string(lcm_robot_state_channel, "EST_ROBOT_STATE",
+              "Channel on which to listen for robot_state_t messages.");
+DEFINE_string(lcm_plan_channel, "COMMITTED_ROBOT_PLAN",
+              "Channel on which to send robot_plan_t messages.");
+DEFINE_double(x, 0.3, "x coordinate (meters) to move to");
+DEFINE_double(y, -0.26, "y coordinate (meters) to move to");
+DEFINE_double(z, 0.5, "z coordinate (meters) to move to");
+DEFINE_double(roll, -1.7,
+              "target roll (radians) about world x axis for end effector");
+DEFINE_double(pitch, -1.3,
+              "target pitch (radians) about world y axis for end effector");
+DEFINE_double(yaw, -1.8,
+              "target yaw (radians) about world z axis for end effector");
+DEFINE_string(ee_name, "panda_link8", "Name of the end effector link");
+
+namespace drake {
+namespace examples {
+namespace franka_panda_arm {
+namespace {
+
+using manipulation::planner::ConstraintRelaxingIk;
+
+const char kUrdfPath[] =
+    "drake/manipulation/models/franka_description/urdf/panda_arm.urdf";
+
+class MoveDemoRunner {
+ public:
+  MoveDemoRunner()
+      : plant_(0.0) {
+    urdf_ =
+        (!FLAGS_urdf.empty() ? FLAGS_urdf : FindResourceOrThrow(kUrdfPath));
+    multibody::Parser(&plant_).AddModelFromFile(urdf_);
+    plant_.WeldFrames(plant_.world_frame(),
+                      plant_.GetFrameByName("panda_link0"));
+    plant_.Finalize();
+    context_ = plant_.CreateDefaultContext();
+
+    lcm_.subscribe(FLAGS_lcm_robot_state_channel,
+                   &MoveDemoRunner::HandleStatus, this);
+
+    joint_names_ = manipulation::util::GetJointNames(plant_);
+  }
+
+  void Run() {
+    while (lcm_.handle() >= 0) { }
+  }
+
+ private:
+  // Handle the incoming status message from the iiwa.  This is a
+  // fairly high rate operation (1000Hz is typical).  The plan is
+  // calculated on the first status message received, after that
+  // periodically display the current position of the end effector.
+  void HandleStatus(const ::lcm::ReceiveBuffer*, const std::string&,
+                    const bot_core::robot_state_t* status) {
+    status_count_++;
+    Eigen::VectorXd panda_q(status->num_joints);
+    for (int i = 0; i < status->num_joints; i++) {
+      panda_q[i] = status->joint_position[i];
+    }
+
+    // Only print the position every 500 messages (this results in an
+    // 0.5s period in a typical configuration).
+    if (status_count_ % 500 == 1) {
+      plant_.SetPositions(context_.get(), panda_q);
+
+      const math::RigidTransform<double> ee_pose =
+          plant_.EvalBodyPoseInWorld(
+              *context_, plant_.GetBodyByName(FLAGS_ee_name));
+      const math::RollPitchYaw<double> rpy(ee_pose.rotation());
+      drake::log()->info("End effector at: {} {}",
+                         ee_pose.translation().transpose(),
+                         rpy.vector().transpose());
+    }
+
+    // If this is the first status we've received, calculate a plan
+    // and send it (if it succeeds).
+    if (status_count_ == 1) {
+      ConstraintRelaxingIk ik(urdf_, FLAGS_ee_name);
+
+      // Create a single waypoint for our plan (the destination).
+      // This results in a trajectory with two knot points (the
+      // current pose (read from the status message currently being
+      // processes and passed directly to PlanSequentialTrajectory as
+      // panda_q) and the calculated final pose).
+      ConstraintRelaxingIk::IkCartesianWaypoint wp;
+      wp.pose.set_translation(Eigen::Vector3d(FLAGS_x, FLAGS_y, FLAGS_z));
+      const math::RollPitchYaw<double> rpy(FLAGS_roll, FLAGS_pitch, FLAGS_yaw);
+      wp.pose.set_rotation(rpy);
+      wp.constrain_orientation = true;
+      std::vector<ConstraintRelaxingIk::IkCartesianWaypoint> waypoints;
+      waypoints.push_back(wp);
+      std::vector<Eigen::VectorXd> q_sol;
+      const bool result =
+          ik.PlanSequentialTrajectory(waypoints, panda_q, &q_sol);
+      drake::log()->info("IK result: {}", result);
+
+      if (result) {
+        drake::log()->info("IK sol size {}", q_sol.size());
+
+        // Run the resulting plan over 2 seconds (which is a fairly
+        // arbitrary choice).
+        std::vector<double> times{0, 2};
+        DRAKE_DEMAND(q_sol.size() == times.size());
+
+        // Convert the resulting waypoints into the format expected by
+        // EncodeKeyFrames.
+        MatrixX<double> q_mat(q_sol.front().size(), q_sol.size());
+        for (size_t i = 0; i < q_sol.size(); ++i) {
+          q_mat.col(i) = q_sol[i];
+        }
+
+        std::vector<int> info{1, 1};
+        robotlocomotion::robot_plan_t plan =
+            manipulation::util::EncodeKeyFrames(
+                joint_names_, times, info, q_mat);
+        lcm_.publish(FLAGS_lcm_plan_channel, &plan);
+      }
+    }
+  }
+
+  ::lcm::LCM lcm_;
+  std::string urdf_;
+  multibody::MultibodyPlant<double> plant_;
+  std::unique_ptr<systems::Context<double>> context_;
+  std::vector<std::string> joint_names_;
+  int status_count_{0};
+};
+
+}  // namespace
+}  // namespace franka_panda_arm
+}  // namespace examples
+}  // namespace drake
+
+int main(int argc, char* argv[]) {
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  drake::examples::franka_panda_arm::MoveDemoRunner runner;
+  runner.Run();
+}
---
+++ examples/franka_panda_arm/panda_controller.cc
@@ -0,0 +1,328 @@
+/// @file
+///
+/// Implements a controller for a Franka Panda arm.
+
+#include <memory>
+
+#include <bot_core/joint_state_t.hpp>
+#include <bot_core/robot_state_t.hpp>
+#include <gflags/gflags.h>
+#include <lcm/lcm-cpp.hpp>
+#include <robotlocomotion/robot_plan_t.hpp>
+
+#include "drake/common/drake_assert.h"
+#include "drake/common/find_resource.h"
+#include "drake/common/text_logging.h"
+#include "drake/lcm/drake_lcm.h"
+#include "drake/manipulation/planner/robot_plan_interpolator.h"
+#include "drake/systems/analysis/simulator.h"
+#include "drake/systems/controllers/pid_controller.h"
+#include "drake/systems/framework/context.h"
+#include "drake/systems/framework/diagram.h"
+#include "drake/systems/framework/diagram_builder.h"
+#include "drake/systems/lcm/lcm_publisher_system.h"
+#include "drake/systems/lcm/lcm_subscriber_system.h"
+#include "drake/systems/primitives/adder.h"
+#include "drake/systems/primitives/demultiplexer.h"
+#include "drake/systems/primitives/multiplexer.h"
+
+using robotlocomotion::robot_plan_t;
+
+DEFINE_string(urdf, "", "Name of urdf to load");
+
+namespace drake {
+namespace examples {
+namespace franka_panda_arm {
+namespace {
+
+/// Handles lcmt_jaco_status messages from a LcmSubscriberSystem.
+///
+/// Note that this system does not actually subscribe to an LCM channel. To
+/// receive the message, the input of this system should be connected to a
+/// systems::lcm::LcmSubscriberSystem::Make<robot_state_t>().
+///
+/// This system has one abstract-valued input port of type robot_state_t.
+///
+/// This system has one vector valued output port which reports
+/// measured position and velocity for each joint and finger.
+///
+/// All ports will continue to output their initial state (typically
+/// zero) until a message is received.
+class RobotStateReceiver : public systems::LeafSystem<double> {
+ public:
+  DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN(RobotStateReceiver)
+
+  explicit RobotStateReceiver(int num_joints);
+
+  /// @name Named accessors for this System's input and output ports.
+  //@{
+  const systems::InputPort<double>& get_input_port() const;
+  const systems::OutputPort<double>& get_output_port() const;
+  //@}
+
+ private:
+  void CalcOutput(const systems::Context<double>&,
+                  systems::BasicVector<double>*) const;
+
+  const int num_joints_;
+};
+
+using systems::BasicVector;
+using systems::Context;
+
+RobotStateReceiver::RobotStateReceiver(int num_joints)
+    : num_joints_(num_joints) {
+  this->DeclareAbstractInputPort(
+      "robot_state_t", Value<bot_core::robot_state_t>{});
+  this->DeclareVectorOutputPort(
+      "state", BasicVector<double>(num_joints_ * 2),
+      &RobotStateReceiver::CalcOutput);
+}
+
+const systems::InputPort<double>& RobotStateReceiver::get_input_port() const {
+  return LeafSystem<double>::get_input_port(0);
+}
+const systems::OutputPort<double>& RobotStateReceiver::get_output_port() const {
+  return LeafSystem<double>::get_output_port(0);
+}
+
+void RobotStateReceiver::CalcOutput(
+    const Context<double>& context, BasicVector<double>* output) const {
+  const auto& status =
+      get_input_port().Eval<bot_core::robot_state_t>(context);
+
+  // If we're using a default constructed message (i.e., we haven't received
+  // any status message yet), output zero.
+  if (status.num_joints == 0) {
+    output->get_mutable_value().setZero();
+  } else {
+    DRAKE_THROW_UNLESS(status.num_joints == num_joints_);
+    DRAKE_THROW_UNLESS(
+        static_cast<int>(status.joint_position.size()) == num_joints_);
+    DRAKE_THROW_UNLESS(
+        static_cast<int>(status.joint_velocity.size()) == num_joints_);
+    auto out = output->get_mutable_value();
+
+    for (int i = 0; i < num_joints_; i++) {
+      out(i) = status.joint_position[i];
+      out(i + num_joints_) = status.joint_velocity[i];
+    }
+  }
+}
+
+/// Creates and outputs lcmt_jaco_command messages
+///
+/// Note that this system does not actually send the message an LCM channel. To
+/// send the message, the output of this system should be connected to a
+/// systems::lcm::LcmPublisherSystem::Make<lcmt_iiwa_command>().
+///
+/// This system has one vector-valued input port containing the
+/// desired position and velocity.
+///
+/// This system has one abstract-valued output port of type
+/// bot_core::joint_state_t;
+///
+class JointStateSender : public systems::LeafSystem<double> {
+ public:
+  DRAKE_NO_COPY_NO_MOVE_NO_ASSIGN(JointStateSender)
+
+  explicit JointStateSender(int num_joints);
+
+  /// @name Named accessors for this System's input and output ports.
+  //@{
+  const systems::InputPort<double>& get_input_port() const;
+  const systems::OutputPort<double>& get_output_port() const;
+  //@}
+
+ private:
+  void CalcOutput(const systems::Context<double>&,
+                  bot_core::joint_state_t*) const;
+
+  const int num_joints_;
+};
+
+JointStateSender::JointStateSender(int num_joints)
+    : num_joints_(num_joints) {
+  this->DeclareInputPort(
+      "state", systems::kVectorValued, num_joints_ * 2);
+  this->DeclareAbstractOutputPort(
+      "joint_state_t", &JointStateSender::CalcOutput);
+}
+
+const systems::InputPort<double>& JointStateSender::get_input_port() const {
+  return LeafSystem<double>::get_input_port(0);
+}
+const systems::OutputPort<double>& JointStateSender::get_output_port() const {
+  return LeafSystem<double>::get_output_port(0);
+}
+
+void JointStateSender::CalcOutput(
+    const systems::Context<double>& context,
+    bot_core::joint_state_t* output) const {
+  const auto& state = get_input_port().Eval(context);
+
+  output->utime = context.get_time() * 1e6;
+  output->num_joints = num_joints_;
+  output->joint_name.resize(num_joints_);
+  output->joint_position.resize(num_joints_);
+  output->joint_velocity.resize(num_joints_);
+  output->joint_effort.resize(num_joints_);
+  for (int i = 0; i < num_joints_; ++i) {
+    output->joint_position[i] = state[i];
+    output->joint_velocity[i] = state[i + num_joints_];
+    output->joint_effort[i] = 0;
+  }
+}
+
+using manipulation::planner::RobotPlanInterpolator;
+
+const char* const kUrdfPath =
+    "drake/manipulation/models/franka_description/urdf/panda_arm.urdf";
+const char* const kLcmStatusChannel = "EST_ROBOT_STATE";
+const char* const kLcmCommandChannel = "FRANKA_COMMAND";
+const char* const kLcmPlanChannel = "COMMITTED_ROBOT_PLAN";
+
+int DoMain() {
+  lcm::DrakeLcm lcm;
+  systems::DiagramBuilder<double> builder;
+
+  auto plan_sub =
+      builder.AddSystem(systems::lcm::LcmSubscriberSystem::Make<robot_plan_t>(
+          kLcmPlanChannel, &lcm));
+
+  const std::string urdf =
+      (!FLAGS_urdf.empty() ? FLAGS_urdf : FindResourceOrThrow(kUrdfPath));
+  auto plan_source = builder.AddSystem<RobotPlanInterpolator>(urdf);
+
+  builder.Connect(plan_sub->get_output_port(),
+                  plan_source->get_plan_input_port());
+  const int num_joints = 7;
+  DRAKE_DEMAND(plan_source->plant().num_positions() == num_joints);
+  DRAKE_DEMAND(plan_source->plant().num_velocities() == num_joints);
+
+  // The driver is operating in joint velocity mode, so that's the
+  // meaningful part of the command message we'll eventually
+  // construct.  We create a pid controller which calculates
+  //
+  // y = kp * (q_desired - q) + kd * (v_desired - v)
+  //
+  // (feedback term) which we'll add to v_desired from the plan source
+  // (feed forward term).
+  Eigen::VectorXd panda_kp = Eigen::VectorXd::Zero(num_joints);
+  Eigen::VectorXd panda_ki = Eigen::VectorXd::Zero(num_joints);
+  Eigen::VectorXd panda_kd = Eigen::VectorXd::Zero(num_joints);
+
+  // I (sam.creasey) have no idea what would be good values here.
+  // This seems to work OK at the low speeds of the jaco.
+  panda_kp.head(num_joints).fill(10);
+  panda_kd.head(num_joints).fill(1);
+
+  panda_kp.head(num_joints).fill(1);
+  panda_kd.head(num_joints).fill(0);
+
+  auto pid_controller = builder.AddSystem<systems::controllers::PidController>(
+      panda_kp, panda_ki, panda_kd);
+
+  // We'll directly fix the input to the status receiver later from our lcm
+  // subscriber.
+  auto status_receiver = builder.AddSystem<RobotStateReceiver>(num_joints);
+
+  builder.Connect(status_receiver->get_output_port(),
+                  pid_controller->get_input_port_estimated_state());
+  builder.Connect(plan_source->get_output_port(0),
+                  pid_controller->get_input_port_desired_state());
+
+  // Split the plan source into q_d (sent in the command message for
+  // informational purposes) and v_d (feed forward term for control).
+  auto target_demux =
+      builder.AddSystem<systems::Demultiplexer>(num_joints * 2, num_joints);
+  builder.Connect(plan_source->get_output_port(0),
+                  target_demux->get_input_port(0));
+
+  // Sum the outputs of the pid controller and v_d.
+  auto adder = builder.AddSystem<systems::Adder>(2, num_joints);
+  builder.Connect(pid_controller->get_output_port_control(),
+                  adder->get_input_port(0));
+  builder.Connect(target_demux->get_output_port(1),
+                  adder->get_input_port(1));
+
+  // Multiplex the q_d and velocity command streams back into a single
+  // port.
+  std::vector<int> mux_sizes(2, num_joints);
+  auto command_mux = builder.AddSystem<systems::Multiplexer>(mux_sizes);
+  builder.Connect(target_demux->get_output_port(0),
+                  command_mux->get_input_port(0));
+  builder.Connect(adder->get_output_port(),
+                  command_mux->get_input_port(1));
+
+  auto command_pub = builder.AddSystem(
+      systems::lcm::LcmPublisherSystem::Make<bot_core::joint_state_t>(
+          kLcmCommandChannel, &lcm));
+  auto command_sender = builder.AddSystem<JointStateSender>(num_joints);
+  builder.Connect(command_mux->get_output_port(0),
+                  command_sender->get_input_port());
+  builder.Connect(command_sender->get_output_port(),
+                  command_pub->get_input_port());
+
+  auto owned_diagram = builder.Build();
+  const systems::Diagram<double>* diagram = owned_diagram.get();
+  systems::Simulator<double> simulator(std::move(owned_diagram));
+
+  // Wait for the first message.
+  drake::log()->info("Waiting for first robot status");
+  lcm::Subscriber<bot_core::robot_state_t> status_sub(&lcm, kLcmStatusChannel);
+  LcmHandleSubscriptionsUntil(&lcm, [&]() { return status_sub.count() > 0; });
+
+  const bot_core::robot_state_t& first_status = status_sub.message();
+  DRAKE_DEMAND(first_status.num_joints == 0 ||
+               first_status.num_joints == num_joints);
+
+  VectorX<double> q0 = VectorX<double>::Zero(num_joints);
+  for (int i = 0; i < first_status.num_joints; ++i) {
+    q0(i) = first_status.joint_position[i];
+  }
+
+  systems::Context<double>& diagram_context = simulator.get_mutable_context();
+  const double t0 = first_status.utime * 1e-6;
+  diagram_context.SetTime(t0);
+
+  auto& plan_source_context =
+      diagram->GetMutableSubsystemContext(*plan_source, &diagram_context);
+  plan_source->Initialize(t0, q0,
+                          &plan_source_context.get_mutable_state());
+
+  systems::Context<double>& status_context =
+      diagram->GetMutableSubsystemContext(*status_receiver, &diagram_context);
+  auto& status_value = status_receiver->get_input_port().FixValue(
+      &status_context, first_status);
+
+  // Run forever, using the bot_core::robot_state_t message to dictate when
+  // simulation time advances.  The robot_plan_t message is handled whenever
+  // the next bot_core::robot_state_t occurs.
+  drake::log()->info("Controller started");
+  while (true) {
+    // Wait for an bot_core::robot_state_t message.
+    status_sub.clear();
+    LcmHandleSubscriptionsUntil(&lcm, [&]() { return status_sub.count() > 0; });
+    // Write the bot_core::robot_state_t message into the context and advance.
+    status_value.GetMutableData()->set_value(status_sub.message());
+    const double time = status_sub.message().utime * 1e-6;
+    simulator.AdvanceTo(time);
+    // Force-publish the lcmt_jaco_command (via the command_pub system within
+    // the diagram).
+    diagram->Publish(diagram_context);
+  }
+
+  // We should never reach here.
+  return EXIT_FAILURE;
+}
+
+}  // namespace
+}  // namespace franka_panda_arm
+}  // namespace examples
+}  // namespace drake
+
+int main(int argc, char* argv[]) {
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  return drake::examples::franka_panda_arm::DoMain();
+}
