1591e346f412e0a838e4ef88510867567f7bbb94
Sam Creasey <sam.creasey@tri.global>
2021-10-28 15:48:54 -0400

    status receiver

--- manipulation/kinova_jaco/jaco_status_receiver.cc
+++ manipulation/kinova_jaco/jaco_status_receiver.cc
@@ -12,36 +12,48 @@ using systems::Context;
 JacoStatusReceiver::JacoStatusReceiver(int num_joints, int num_fingers)
     : num_joints_(num_joints),
       num_fingers_(num_fingers) {
-  this->DeclareAbstractInputPort(
+  message_input_ = &DeclareAbstractInputPort(
       "lcmt_jaco_status", Value<lcmt_jaco_status>{});
-  this->DeclareVectorOutputPort("state", (num_joints_ + num_fingers_) * 2,
-                                &JacoStatusReceiver::CalcStateOutput);
-  this->DeclareVectorOutputPort(
-      "torque", num_joints_ + num_fingers_,
-      &JacoStatusReceiver::CalcLcmOutput<&lcmt_jaco_status::joint_torque,
-                                         &lcmt_jaco_status::finger_torque>);
-  this->DeclareVectorOutputPort("torque_external", num_joints_ + num_fingers_,
-                                &JacoStatusReceiver::CalcLcmOutput<
-                                    &lcmt_jaco_status::joint_torque_external,
-                                    &lcmt_jaco_status::finger_torque_external>);
-  this->DeclareVectorOutputPort(
-      "current", num_joints_ + num_fingers_,
-      &JacoStatusReceiver::CalcLcmOutput<&lcmt_jaco_status::joint_current,
-                                         &lcmt_jaco_status::finger_current>);
+  state_output_ = &DeclareVectorOutputPort(
+      "state", (num_joints_ + num_fingers_) * 2,
+      &JacoStatusReceiver::CalcStateOutput);
+  position_measured_output_ = &DeclareVectorOutputPort(
+      "position_measured", num_joints_,
+      &JacoStatusReceiver::CalcJointOutput<&lcmt_jaco_status::joint_position>);
+  velocity_measured_output_ = &DeclareVectorOutputPort(
+      "velocity_measured", num_joints_,
+      &JacoStatusReceiver::CalcJointOutput<&lcmt_jaco_status::joint_velocity>);
+  finger_position_measured_output_ = &DeclareVectorOutputPort(
+      "finger_position_measured", num_fingers_,
+      &JacoStatusReceiver::CalcFingerOutput<
+      &lcmt_jaco_status::finger_position>);
+  finger_velocity_measured_output_ = &DeclareVectorOutputPort(
+      "finger_velocity_measured", num_fingers_,
+      &JacoStatusReceiver::CalcFingerOutput<
+      &lcmt_jaco_status::finger_velocity>);
+  torque_measured_output_ = &DeclareVectorOutputPort(
+      "torque_measured", num_joints_,
+      &JacoStatusReceiver::CalcJointOutput<&lcmt_jaco_status::joint_torque>);
+  torque_external_output_ = &DeclareVectorOutputPort(
+      "torque_external", num_joints_,
+      &JacoStatusReceiver::CalcJointOutput<
+      &lcmt_jaco_status::joint_torque_external>);
+  current_output_ = &DeclareVectorOutputPort(
+      "current", num_joints_,
+      &JacoStatusReceiver::CalcJointOutput<&lcmt_jaco_status::joint_current>);
 }
 
-using OutPort = systems::OutputPort<double>;
-const OutPort& JacoStatusReceiver::get_state_output_port() const {
-  return LeafSystem<double>::get_output_port(0);
-}
-const OutPort& JacoStatusReceiver::get_torque_output_port() const {
-  return LeafSystem<double>::get_output_port(1);
-}
-const OutPort& JacoStatusReceiver::get_torque_external_output_port() const {
-  return LeafSystem<double>::get_output_port(2);
-}
-const OutPort& JacoStatusReceiver::get_current_output_port() const {
-  return LeafSystem<double>::get_output_port(3);
+const systems::OutputPort<double>&
+JacoStatusReceiver::get_state_output_port() const {
+  if (num_fingers_) {
+    static const logging::Warn log_once(
+        "The state output port on JacoStatusReceiver is deprecated.  As part "
+        "of this change, the size of the torque, torque_external, and "
+        "current output ports has changed.  If your program is using those "
+        "along with the deprecated API, this may cause problems.");
+  }
+
+  return *state_output_;
 }
 
 void JacoStatusReceiver::CalcStateOutput(
@@ -76,9 +88,8 @@ void JacoStatusReceiver::CalcStateOutput(
   output->get_mutable_value() = state;
 }
 
-template <std::vector<double> drake::lcmt_jaco_status::* arm_ptr,
-          std::vector<double> drake::lcmt_jaco_status::* finger_ptr>
-void JacoStatusReceiver::CalcLcmOutput(
+template <std::vector<double> drake::lcmt_jaco_status::* field_ptr>
+void JacoStatusReceiver::CalcJointOutput(
     const Context<double>& context, BasicVector<double>* output) const {
   const auto& status = get_input_port().Eval<lcmt_jaco_status>(context);
 
@@ -89,15 +100,26 @@ void JacoStatusReceiver::CalcLcmOutput(
     return;
   }
 
-  Eigen::VectorXd state(num_joints_ + num_fingers_);
-  const auto& arm_field = status.*arm_ptr;
-  state.head(num_joints_) = Eigen::Map<const Eigen::VectorXd>(
+  const auto& arm_field = status.*field_ptr;
+  output->get_mutable_value() = Eigen::Map<const Eigen::VectorXd>(
       arm_field.data(), arm_field.size());
+}
 
-  const auto& finger_field = status.*finger_ptr;
-  state.tail(num_fingers_) = Eigen::Map<const Eigen::VectorXd>(
-      finger_field.data(), finger_field.size());
-  output->get_mutable_value() = state;
+template <std::vector<double> drake::lcmt_jaco_status::* field_ptr>
+void JacoStatusReceiver::CalcFingerOutput(
+    const Context<double>& context, BasicVector<double>* output) const {
+  const auto& status = get_input_port().Eval<lcmt_jaco_status>(context);
+
+  // If we're using a default constructed message (i.e., we haven't received
+  // any status message yet), output zero.
+  if (status.num_fingers == 0) {
+    output->get_mutable_value().setZero();
+    return;
+  }
+
+  const auto& finger_field = status.*field_ptr;
+  output->get_mutable_value() = Eigen::Map<const Eigen::VectorXd>(
+      finger_field.data(), finger_field.size()) * kFingerSdkToUrdf;
 }
 
 }  // namespace kinova_jaco
